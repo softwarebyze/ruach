c3e97eb77e5a2cfdb2b195c64cbc88ae9b916ab0
softwarebyze <zackebenfeld@gmail.com>
2023-12-10 16:47:56 -0500

    tailwind setup, location autocomplete, responsive nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
+import { getAutocompleteLocations } from "./api/autocomplete";
+import { Nav } from "./components/Nav";
 
 function App() {
+  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.currentTarget.value;
+    const results = await getAutocompleteLocations(query, true);
+    console.log(results);
+  };
   return (
-    <div>
-      <h1>Weather</h1>
-    </div>
+    <>
+      <Nav />
+      <div>
+        <nav></nav>
+        <h1 className="mb-9">Weather</h1>
+        <input
+          className="p-2 rounded-xl"
+          type="search"
+          placeholder="Search for a city"
+          onChange={handleSearch}
+        />
+      </div>
+    </>
   );
 }
 
